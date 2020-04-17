653718fadcbbc37c9cf274e82688b129c96fb6d7
Hansong Li@SEG <hansong.li@seg-automotive.com>
2020-04-17 12:46:26 +0800

    Setup reader

---
+++ Excel_DBC_Template_Reader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Excel_DBC_Template_Reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
